917ae1d1d0f1be28e53a2cfc2dddd4f21297480c
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2024-12-22 02:51:58 +0530

    added init commands

--- src/cmd/commands/__init__.py
+++ src/cmd/commands/__init__.py
@@ -1,3 +1,5 @@
 from .embed import EmbedCommand
+from .api_key import APIKeyCommand
+from .init_proj import InitProjectCommand
 
-__all__ = ['EmbedCommand']
+__all__ = ['EmbedCommand', 'APIKeyCommand', 'InitProjectCommand']
