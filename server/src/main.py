05bbc6a4eb142e6c99428a22b78b4bdca38301b3
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2024-12-24 19:49:54 +0530

    added uvicorn

--- server/src/main.py
+++ server/src/main.py
@@ -1,5 +1,22 @@
-from cmd.command import cli
+import os
 
 
 if __name__ == "__main__":
-    cli()
+    import uvicorn
+
+    if os.environ.get("ENV") == "prod":
+        uvicorn.run(
+            "web:fastapi_app",
+            host="0.0.0.0",
+            port=8000,
+            reload=False,
+            log_level="info",
+        )
+    else:
+        uvicorn.run(
+            "web:fastapi_app",
+            host="0.0.0.0",
+            port=8000,
+            reload=True,
+            log_level="info",
+        )
