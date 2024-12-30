c713b5543cc233c0983d1b2d87dbaa2cee8ba96e
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2024-12-31 04:21:12 +0530

    better readability

--- server/src/config/__init__.py
+++ server/src/config/__init__.py
@@ -1,6 +1,11 @@
-from .project import ProjectManager, init_project
-from .configs import ProjectConfig, EmbeddingConfig, LLMConfig, create_sample_config_file, sample_config, VectorConfig
+from .project import ProjectManager
+from .configs import (ProjectConfig,
+                      EmbeddingConfig,
+                      LLMConfig,
+                      create_sample_config_file,
+                      sample_config,
+                      VectorConfig)
 from .api import APIProvider, APIKeyManager, verify_provider
 
 __all__ = ['ProjectManager', 'ProjectConfig', 'EmbeddingConfig',
-           'LLMConfig', 'APIProvider', 'APIKeyManager', 'create_sample_config_file', 'init_project', 'sample_config', 'VectorConfig', 'verify_provider']
+           'LLMConfig', 'APIProvider', 'APIKeyManager', 'create_sample_config_file', 'sample_config', 'VectorConfig', 'verify_provider']
